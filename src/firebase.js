0b6835e97293ebb047616cf88265e65a5553fb12
Jake <38390615+jakedoesgithub@users.noreply.github.com>
2019-02-28 12:36:16 -0600

    Switch over to React

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAuESVoqcb8AdYx4Xra6hBXPX0ZL6UvDIY",
+  authDomain: "tigerparking.firebaseapp.com",
+  databaseURL: "https://tigerparking.firebaseio.com",
+  projectId: "tigerparking",
+  storageBucket: "tigerparking.appspot.com",
+  messagingSenderId: "360909428304"
+};
+firebase.initializeApp(config);
